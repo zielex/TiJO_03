881a85a9bffd20a886439e17a9d38da4151dd831
aniaw <ania_w@outolook.com>
2016-03-01 19:58:15 +0100

    update areaOfTrapezoid function

--- app/app.js
+++ app/app.js
@@ -5,7 +5,10 @@
     window.exercise1 = {
         areaOfTrapezoid: function (a, b, h)
         {
-
+            if (a < 0 || b < 0 || h < 0 || typeof a !== 'number' || typeof b !== 'number' || typeof h !== 'number') {
+                return false;
+            }
+            return 0.5 * h * (a + b);
         }
     };
 })();
